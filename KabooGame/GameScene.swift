29169479560ed7a584c8bc05a71746ec403e45c9
Lucas Pereira <lucasgpereira.10@gmail.com>
2022-02-10 18:02:09 +0100

    Adding view with GameScene

---
+++ KabooGame/GameScene.swift
@@ -0,0 +1,19 @@
+//
+//  GameScene.swift
+//  KabooGame
+//
+//  Created by Lucas Pereira on 10/02/22.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    override func didMove(to view: SKView) {
+        // Run when scene begins
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        // Run when touch is detected
+    }
+}
+
