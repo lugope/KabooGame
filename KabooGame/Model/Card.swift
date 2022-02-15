c5959f381a095220fabe4d7c399430ade969246a
Lucas Pereira <lucasgpereira.10@gmail.com>
2022-02-15 15:33:11 +0100

    Refining card data structure and adding facedown image

--- KabooGame/Model/Card.swift
+++ KabooGame/Model/Card.swift
@@ -8,26 +8,38 @@
 import Foundation
 import SpriteKit
 
+let CARD_FACEDOWN_TEXTURE = SKTexture(imageNamed: "Card_facedown")
+
 class Card: SKSpriteNode {
-    let cardId: Int
-    let pointsValue: Int
+    let type: CardType
+    var faceUp: Bool = true
     
-    init(cardId: Int) {
-        self.cardId = cardId
-        if cardId <= 13 {
-            self.pointsValue = cardId
-        } else {
-            self.pointsValue = -1
-        }
+    init(cardType: CardType) {
+        self.type = cardType
         
-        let textureName = "Card_" + String(cardId)
-        let texture = SKTexture(imageNamed: textureName)
-        
-        super.init(texture: texture, color: UIColor.clear, size: texture.size())
+        super.init(texture: self.type.texture, color: UIColor.clear, size: self.type.texture.size())
         self.name = "Card"
     }
     
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+}
+
+enum CardType: Int {
+    case card0 = 0, card1, card2, card3, card4, card5, card6, card7, card8, card9, card10, card11, card12, card13, card14
+    
+    var texture: SKTexture {
+        let textureName = "Card_" + String(self.rawValue)
+        return SKTexture(imageNamed: textureName)
+    }
+    
+    var value: Int {
+        if self.rawValue <= 13 {
+            return self.rawValue
+            
+        } else {
+            return -1
+        }
     }
 }
